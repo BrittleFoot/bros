74313f3c9deabc6fb3c8dce3917bb1a7c1509337
Igor Ostanin <igor.ostanin@astrazeneca.com>
2022-06-27 20:08:51 +0400

    Tailwind added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-center">
+    Welcome to SvelteKit
+</h1>
+<h1 class="text-center text-3xl font-bold underline">
+    Hello tailwind!
+</h1>
